2ac46b69c2c06a1dbb64232ec121a84a6d5145f8
Jeremy <jeremyash13@gmail.com>
2020-06-11 13:00:58 -0600

    .

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Library from "./components/Library";
 import StoreContext from "./containers/StoreContext";
 import ReactHowler from "react-howler";
 import { useLocalStore, useObserver } from "mobx-react";
-
-const { ipcRenderer } = require('electron')
+import Sound from 'react-sound';
+import mp3url from './test-music/Overnight.mp3'
+// const { ipcRenderer } = require('electron')
 
 const StoreProvider = ({ children }) => {
   const store = useLocalStore(() => ({
@@ -34,14 +35,20 @@ const StoreProvider = ({ children }) => {
 
 export default function App() {
   useEffect(() => {
-    console.log(ipcRenderer.send('init-library', 'ping'))
+    // console.log(ipcRenderer.send('init-library', 'ping'))
   }, [])
+
+  const soundProps = {
+    url: mp3url,
+    playStatus: Sound.status.PLAYING
+  }
+
   return (
     <StoreProvider>
       <div className="font-jost text-white bg-dark-blue h-screen flex flex-col p-8">
         <Library />
         <PlayBar />
-        <ReactHowler src="../public/test-music/Overnight.mp3" playing={true} />
+        <Sound {...soundProps} />
       </div>
     </StoreProvider>
   );
--- src/electron-starter.js
+++ src/electron-starter.js
@@ -7,7 +7,7 @@ const BrowserWindow = electron.BrowserWindow;
 
 const path = require("path");
 const url = require("url");
-const {ipcMain} = require("electron");
+// const {ipcMain} = require("electron");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -70,8 +70,8 @@ app.on("activate", function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-ipcMain.on('init-library', (event, arg) => {
-    populateLibrary()
-    console.log(arg)
-    event.returnValue = 'pong'
-  })
+// ipcMain.on('init-library', (event, arg) => {
+//     populateLibrary()
+//     console.log(arg)
+//     event.returnValue = 'pong'
+//   })
