9c1fcb612874734697f7bef388b8c75476552e41
Jeremy <jeremyash13@gmail.com>
2020-07-08 20:02:11 -0600

    added icon for SongContextMenu

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -2,9 +2,8 @@ import React from "react";
 
 export default function Marquee({ children }) {
   return (
-    <div className="">
-      <div className="marquee text-2xl font-euclid-semibold text-horizon-red text-glow-red-2">{children}</div>
-      {/* <div className="marquee-2 text-horizon-red text-glow-red-2">{children}</div> */}
+    <div className="w-64">
+      <div className="marquee w-full text-2xl font-euclid-semibold text-horizon-red text-glow-red-2">{children}</div>
     </div>
   );
 }
--- src/components/SongContextMenu.js
+++ src/components/SongContextMenu.js
@@ -5,13 +5,44 @@ export default function SongContextMenu() {
   return (
     <>
       {isOpen ? (
-        <div className="absolute flyout border border-gray-700 z-50 transform -translate-x-8 opacity-0 bg-horizon-gray w-48 py-2 px-4 rounded text-right">
-          <div className="">+ Playlist</div>
-          <div className="">+ Queue</div>
+        <div className="absolute text-base flyout border border-gray-700 z-50 transform -translate-x-8 opacity-0 bg-horizon-gray w-40 py-2 px-4 rounded text-right">
+          <div className="p-1">+ Playlist</div>
+          <div className="p-1">+ Queue</div>
         </div>
       ) : (
-        <div className="absolute transform -translate-x-10 -translate-y-1 bg-black px-2" onClick={() => {setIsOpen(true)}}>
-          <div className="text-xl">+</div>
+        <div
+          className="absolute w-5 h-5 text-horizon-gray transform -translate-x-10 bg-transparent"
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
+          <div className="w-full m-auto">
+            <svg
+              version="1.1"
+              id="Capa_1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 384 384"
+              className="h-full"
+              fill="currentColor"
+            >
+              <g>
+                <g >
+                  <circle cx="192" cy="42.667" r="42.667"/>
+                </g>
+              </g>
+              <g>
+                <g>
+                  <circle cx="192" cy="192" r="42.667" />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <circle cx="192" cy="341.333" r="42.667" />
+                </g>
+              </g>
+            </svg>
+          </div>
         </div>
       )}
     </>
